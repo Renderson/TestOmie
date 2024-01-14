236a69dc26bee7e74ef0f25e9f9b1c20e5489283
renderson <renderson.silva@southsystem.com>
2024-01-14 17:38:24 -0300

    atualizado model para ser a tabela do room

--- app/src/main/java/com/renderson/testomie/model/Products.kt
+++ app/src/main/java/com/renderson/testomie/model/Products.kt
@@ -1,11 +1,17 @@
 package com.renderson.testomie.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
 data class Products(
+    @PrimaryKey(autoGenerate = true)
+    val productId: Int? = 0,
     var name: String? = null,
     var amount: String = "",
     var unitaryValue: String = "",
 ) {
-    var valorTotal: Double = 0.0
+    val valorTotal: Double
         get() = if (amount.isNotEmpty() && unitaryValue.isNotEmpty()) {
             amount.toDouble() * unitaryValue.toDouble()
         } else 0.0
